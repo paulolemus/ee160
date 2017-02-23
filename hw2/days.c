e45ed4594d4d77a14ad1bddeb16f383b7f235479
paulolemus <paulolemus14@gmail.com>
2017-02-22 17:02:02 -1000

    Cleaned up days.* and driver2.c

--- hw2/days.c
+++ hw2/days.c
@@ -1,38 +1,25 @@
-//      File:   days.c
-//      By:     Kevin Liu
-//      login:  liukevin
-//      Team:   AMERICA
-//      Members:Paulo, Christopher
-//      Date:   02/21/17
+/* File   : days.c
+ * By     : Kevin Liu, Paulo Lemus
+ * login  : liukevin
+ * Team   : AMERICA
+ * Members: Paulo, Christopher
+ * Date   : 02/21/17
+ */
 
-/*      Function Definiton to get numbers of days in that month */
+/* Function Definiton to get numbers of days in that month
+ *
+ * Status: FINISHED, needs comments
+ */
 
 #include "days.h"
+#include "leap.h"
 
-int days_in_month(int month, int year)
-{
-        if (month <= 7) //when months July and before 
-        {
-                if (month % 2 != 0) //if odd months (Jan, Mar, May, July)
-                        return 31;
-                else //if even months (Feb, Apr, June)
-                {
-                        if (month == 2) //February
-                        {
-                                if (is_leap(year) == TRUE)
-                                        return 29;
-                                else
-                                        return 28;
-                        }
-                        else //April & June
-                                return 30;
-                }
-        }
-        else //months after July
-        {
-                if (month % 2 != 0) //if odd months (Sept, Nov)
-                        return 30;
-                else //if even months (Aug, Oct, Dec)
-                        return 31;
-        }
+int days_in_month(int month, int year){
+    int days = 0;
+    
+    days = (month % 2 == 1 && month < 8) ? 31 : 30;
+    if(month % 2 != 0 && month > 7) days = 30;
+    if(month % 2 == 0 && month > 7) days = 31;
+    if(month == 2) days = 28 + is_leap(year);
+    return days;
 }
--- hw2/days.h
+++ hw2/days.h
@@ -1,13 +1,20 @@
-//      File:   days.h
-//      By:     Kevin Liu
-//      login:  liukevin
-//      Team:   AMERICA
-//      Members:Paulo, Christopher
+/* File   : days.h
+ * By     : Kevin Liu
+ * login  : liukevin
+ * Team   : AMERICA
+ * Members: Paulo, Christopher
+ */
 
-/*      Function Prototype to check the number of days in that month    */
+/* Function Prototype to check the number of days in that month
+ *
+ * Status: FINISHED
+ */
 
-#include "leap.h"
+#ifndef EE_DAYS_H_
+#define EE_DAYS_H_
 
 int days_in_month(int month, int year);
 /*      Given: month and year
  *      Returns: number of days in that month   */
+
+#endif
