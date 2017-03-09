e5c91a9d6d922e0a9ced6ba7849dadda62e6563a
Kevin Liu <liukevin@hawaii.edu>
2017-03-08 16:08:01 -1000

    utility function declaration

---
+++ hw3/charutil.h
@@ -0,0 +1,13 @@
+/*      File:   charutil.h
+ *      By:     Kevin Liu
+ *      Login:  liukevin
+ *      Team:   AMERICA
+ *      Members:Paulo, Christopher  */
+
+/*  utility functions declarations  */
+
+int is_vowel(char);
+/*  checks if character is a vowel  */
+
+int is_h_consonant(char);
+/*  checks if consonant  */
