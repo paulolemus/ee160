1d8dc8cf2ed55f898669753b04bf9de714b9c030
Paulo Lemus <paulolemus14@gmail.com>
2017-04-12 18:44:25 -1000

    Added header guards to place.h

--- hw4/place.h
+++ hw4/place.h
@@ -1,11 +1,14 @@
 /*
-file: place.h
-by:
-login:
-date:
-team:
-members:
+ * file     : place.h
+ * by       :
+ * login    :
+ * date     : 4/12/2017
+ * team     : AMERICA
+ * members  : Paulo L, Kevin L, Christopher A
 */
+
+#ifndef EE_HW4_PLACE_H_
+#define EE_HW4_PLACE_H_
 
 #define REALLYRANDOM 0
 
@@ -13,3 +16,6 @@ void place(int *x, int *y);
 /* Generates 2 random integers saves them in the pointers given
  * Given: 2 pointers to intgers
  * Returns: nothing */
+
+
+#endif // EE_HW4_PLACE_H_
