64005942b091992490e717c1aceb81e052c08616
Paulo Lemus <paulolemus14@gmail.com>
2017-04-12 18:42:26 -1000

    Finished place module

--- hw4/place.c
+++ hw4/place.c
@@ -6,13 +6,19 @@
  * team: AMERICA
  * members: Kevin L, Paulo L, Christopher A
  */
-
+#include <stdlib.h>
+#include "display.h"
 #include "place.h"
 
 /* Generates 2 random integers saves them in the pointers given
  * Given: 2 pointers to intgers
- * Returns: nothing */
+ * Returns: nothing 
+ * 
+ * ALGORITHM:
+ * Generate random coordinate
+ * Save coordinate
+ */
 void place(int *x, int *y) {
-
-
+    *x = rand() % WIDTH;
+    *y = rand() % HEIGHT;
 }
--- hw4/place.h
+++ hw4/place.h
@@ -1,11 +1,13 @@
 /*
-file: place.h
-by:
-login:
-date:
-team:
-members:
+ * file: place.h
+ * by:
+ * login:date:
+ * team:
+ * members:
 */
+
+#ifndef EE_HW4_PLACE_H_
+#define EE_HW4_PLACE_H_
 
 #define REALLYRANDOM 0
 
@@ -13,3 +15,6 @@ void place(int *x, int *y);
 /* Generates 2 random integers saves them in the pointers given
  * Given: 2 pointers to intgers
  * Returns: nothing */
+
+
+#endif // EE_HW4_PLACE_H_
