b9f2acfc8f9221b237da53afa78b7d00bde59f67
Paulo Lemus <paulolemus14@gmail.com>
2017-04-12 18:13:52 -1000

    Finished the move module for the player

--- hw4/move.c
+++ hw4/move.c
@@ -5,12 +5,51 @@
  * Team: AMERICA
  */
 
-/* TODO: Write all the functions for the prototypes
- * in the move.h header!
+#include <stdio.h>
+#include "display.h"
+#include "move.h"
+
+
+/* ALGORITHM:
+ * Get player command
+ * Update timmy's position
+ * display Timmy at new position
+ * return command entered
  */
 char move(int* x, int* y) {
+    // Get player command
+    char cmd = getchar();
+    draw_symbol(*x, *y, ' ');
+    
+    switch(cmd) {
+        case 'w':
+        case 'i':
+            *y = *y - 1;
+            if(*y < 0) *y = HEIGHT - 1;
+            break;
 
+        case 'a':
+        case 'j':
+            *x = *x - 1;
+            if(*x < 0) *x = WIDTH - 1;
+            break;
 
+        case 's':
+        case 'k':
+            *y = *y + 1;
+            if(*y >= HEIGHT) *y = 0;
+            break;
+
+        case 'd':
+        case 'l':
+            *x = *x + 1;
+            if(*x >= WIDTH ) *x = 0;
+            break;
+
+        default: break;
+    }
+    // Draw timmy to screen
+    draw_symbol(*x, *y, TIMMY);
+    return cmd;
 }
 
-#include "move.h"
--- hw4/moveTest.c
+++ hw4/moveTest.c
@@ -1,8 +1,8 @@
 /*
  * file    : moveTest.c
- * by      : 
- * login   :
- * date    :
+ * by      : Paulo Lemus
+ * login   : Plemus
+ * date    : 4/12/2017
  * team    : AMERICA
  * members : Paulo L, Kevin L, Christopher A
 */
@@ -15,13 +15,29 @@
 
 int main()
 {
-	draw_map();
+    int timX = 0;
+    int timY = 0;
+    char cmd = 't'; // random
 
-    // TODO: WRITE THIS
+    char* text[10];
+    text[1] = "Welcome! This is a move test program";
+    text[2] = "Press w, a, s, or d to move.";
+    text[3] = "Alternatively, use the i, j, k, l control scheme";
 
-	getchar();//so it doesn't close on you right away
+    draw_map();
+    debug_wds(1, text[1]);
+    debug_wds(2, text[2]);
+    debug_wds(3, text[3]);
+    draw_symbol(timX, timY, TIMMY);
+    display_pos(timX, timY);
+    display_score(-999);
 
-	clear_screen();
+    while(cmd != 'q') {
+        cmd = move(&timX, &timY);
+        display_pos(timX, timY);
+        display_score(-999);
+    }
 
+    clear_screen();
 	return 0;
 }
