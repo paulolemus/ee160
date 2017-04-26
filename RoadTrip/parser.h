9e57a9678761099e8b8f9c6857b418ec73577dbe
paulolemus <paulolemus14@gmail.com>
2017-04-26 12:24:06 -1000

    Added more of the skeleton, added header guards and set up Makefile

---
+++ RoadTrip/parser.h
@@ -0,0 +1,16 @@
+/* File: parser.h
+ * Name:
+ * Date: 4/26/2017
+ * Team: AMERICA
+ */
+
+/*
+ *
+ */
+
+#ifndef AMERICA_PARSER_H_
+#define AMERICA_PARSER_H_
+
+
+
+#endif // AMERICA_PARSER_H_
---
+++ RoadTrip/pathplanning.h
@@ -0,0 +1,16 @@
+/* File: pathplanning.h
+ * Name:
+ * Date: 4/26/2017
+ * Team: AMERICA
+ */
+
+/*
+ *
+ */
+
+#ifndef AMERICA_PATHPLANNING_H_
+#define AMERICA_PATHPLANNING_H_
+
+
+
+#endif // AMERICA_PATHPLANNING_H_
