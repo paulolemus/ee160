4a236600f01d9ff88d8f146c456cfd4b31fb1573
Kevin Liu <liukevin@hawaii.edu>
2017-05-03 20:43:32 -1000

    driver update

--- RoadTrip/RoadTrip.c
+++ RoadTrip/RoadTrip.c
@@ -9,13 +9,21 @@
 #include "Node.h"
 #include "pathplanning.h"
 
-
-
 int main() {
 
+    /* declare & initialize variables */
     int listSize = 8;
+    
+    /* path computations */
     struct Node** adjList  = parseGraph(listSize);
     struct Node* shortPath = shortestPath(adjList, listSize, SAN_FRAN, NEW_YORK);
     struct Node* longPath  = longestPath (adjList, listSize, SAN_FRAN, NEW_YORK);
+
+    printf("The Shortest Path is:\n");
+    printf("%s -> %s -> %s -> %s\n", &shortPath->city, &shortPath->city, &shortPath->city);
+    printf("The Longest Path is:\n");
+    printf("%s -> %s -> %s -> %s\n", &longPath->city, &longPath->city, &longPath->city, &longPath->city);
+    printf("%d");
+
     return 0;
 }
