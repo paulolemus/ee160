ad2f40d52dd8464c9a680b092f8005d8fbc2b922
paulolemus <paulolemus14@gmail.com>
2017-05-03 19:45:41 -1000

    Cleaned the pathplanning.h file

--- RoadTrip/pathplanning.h
+++ RoadTrip/pathplanning.h
@@ -1,5 +1,5 @@
 /* File: pathplanning.h
- * Name:
+ * Name: Paulo L, Kevin L
  * Date: 4/26/2017
  * Team: AMERICA
  */
@@ -13,16 +13,14 @@
 
 #include "Node.h"
 
-static const char* START = "san_francisco";
-static const char* GOAL  = "new_york";
-
 /*
  *
  */
 struct Node* shortestPath(struct Node** adjList, int size, enum City start, enum City goal);
-/*
- *
+/* Given: Node** (adjacency list), size of list, start city and end city enums
+ * Returns: Pointer to a list of the shortest path from start to end.
  */
+
 struct Node* longestPath(struct Node** adjList, int size, enum City start, enum City goal);
 /*  Given:  Node**, size of list, san_francisco, & new_york
  *  Returns: pointer to a Node with longestPath
